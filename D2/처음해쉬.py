e516353512001b8bcf39bb070c7d27b668071782
namth <wase894@gmail.com>
2020-02-11 13:07:55 +0900

    해시처음사용

---
+++ D2/처음해쉬.py
@@ -0,0 +1,17 @@
+##해쉬이용 배열에서 타겟넘버가 되는 두 수의 인덱스를 구하라
+target = int(input("target : "))
+nums = input().split()
+nums = [int(i) for i in nums]
+dic={}
+for i in range(target):
+    dic[i]=-1
+for i in range(len(nums)):
+    key = 0
+    if(target>nums[i]):
+        key = target-nums[i]
+        dic[key] = i
+    if(dic[target-key]!=-1):
+        print("index1 {}, index2 {}".format(dic[target-key],dic[key]))
+        break
+
+    
