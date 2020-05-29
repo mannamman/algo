6b312ee99af6057a121468619b4f928dfd4b2b0b
namth <wase894@gmail.com>
2020-05-29 17:06:31 +0900

    삭제

--- 2차원 배열 가로세로.py
+++
@@ -1,6 +0,0 @@
-temp = [[0, 0, 1, 1, 1], [1, 1, 1, 1, 0], [0, 0, 1, 0, 0], [0, 1, 1, 1, 1], [1, 1, 1, 0, 1]]
-for i in range(5):
-    for j in range(5):
-        print("가로 : ",temp[i][j])
-    for j in range(5):
-        print("세로 : ",temp[j][i])
